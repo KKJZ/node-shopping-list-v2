118b9db39dbcf96f22aad0e29ea62c58035cc0ff
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-07-03 11:29:31 -0700

    req.body returns undefined in post /recipes

--- server.js
+++ server.js
@@ -47,6 +47,21 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (res, req) => {
+  const requiredFields = ["name", "ingredients"];
+  for (let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i];
+    //Problem here that I can't Solve even with soultion, req.body = undefined
+    console.log(req.body);
+    if (!(field in req.body)) {
+      const message =`missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
